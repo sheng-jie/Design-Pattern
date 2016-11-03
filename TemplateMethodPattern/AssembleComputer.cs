1a2587074450f5a9f40119db4f9aa977803ef2c0
jeffrey <ysjshengjie@live.cn>
2016-11-03 12:41:17 +0800

    模板方法模式

---
+++ TemplateMethodPattern/AssembleComputer.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateMethodPattern
+{
+    /// <summary>
+    /// 组装电脑
+    /// </summary>
+    public abstract class AssembleComputer
+    {
+        /// <summary>
+        /// 组装主机
+        /// </summary>
+        public abstract void BuildMainFramePart();
+
+        /// <summary>
+        /// 组装显示器
+        /// </summary>
+        public abstract void BuildScreenPart();
+
+        /// <summary>
+        /// 组装输入设备（键鼠）
+        /// </summary>
+        public abstract void BuildInputPart();
+
+        /// <summary>
+        /// 组装起来
+        /// </summary>
+        public void Assemble()
+        {
+            BuildMainFramePart();
+            BuildScreenPart();
+            BuildInputPart();
+        }
+
+    }
+
+    /// <summary>
+    /// 组装HP电脑
+    /// </summary>
+    public class AssembleHpComputer : AssembleComputer
+    {
+        public override void BuildMainFramePart()
+        {
+            Console.WriteLine("组装HP电脑的主板");
+        }
+
+        public override void BuildScreenPart()
+        {
+            Console.WriteLine("组装HP电脑的显示器");
+        }
+
+        public override void BuildInputPart()
+        {
+            Console.WriteLine("组装HP电脑的键盘鼠标");
+        }
+    }
+
+    /// <summary>
+    /// 组装HP电脑
+    /// </summary>
+    public class AssembleDellComputer : AssembleComputer
+    {
+        public override void BuildMainFramePart()
+        {
+            Console.WriteLine("组装Dell电脑的主板");
+        }
+
+        public override void BuildScreenPart()
+        {
+            Console.WriteLine("组装Dell电脑的显示器");
+        }
+
+        public override void BuildInputPart()
+        {
+            Console.WriteLine("组装Dell电脑的键盘鼠标");
+        }
+    }
+
+
+}
---
+++ TemplateMethodPattern/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateMethodPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //组装HP电脑
+            Console.WriteLine("开始组织Hp电脑:");
+            AssembleComputer assembleHpComputer = new AssembleHpComputer();
+
+            assembleHpComputer.Assemble();
+
+            Console.ReadLine();
+
+            //组装DELL电脑
+            Console.WriteLine("开始组织DELL电脑:");
+            AssembleComputer assembleDellComputer = new AssembleDellComputer();
+
+            assembleDellComputer.Assemble();
+
+            Console.ReadLine();
+        }
+    }
+}
