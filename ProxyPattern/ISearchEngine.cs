6af3acf40b49c253d62143d1356b3ed05445bb29
shengjie_yan <ysjshengjie@live.cn>
2016-11-09 21:22:00 +0800

    代理模式

---
+++ ProxyPattern/ISearchEngine.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProxyPattern
+{
+    public interface ISearchEngine
+    {
+        void Search(string searchStr);
+    }
+
+    public class Google : ISearchEngine
+    {
+        public void Search(string searchStr)
+        {
+            string url = "https://www.google.com/search?q=";
+            var request = (HttpWebRequest)WebRequest.Create(url + searchStr);
+
+            var response = (HttpWebResponse)request.GetResponse();
+
+            if (response != null)
+            {
+                Console.WriteLine(string.Format("搜索【{0}】并成功返回！", searchStr));
+            }
+        }
+    }
+
+    public class GoogleProxy : ISearchEngine
+    {
+        private readonly ISearchEngine _vpn = null;
+
+        public GoogleProxy()
+        {
+            _vpn = new Google();
+        }
+
+        public void Search(string searchStr)
+        {
+            try
+            {
+                _vpn.Search(searchStr);
+            }
+            catch (Exception)
+            {
+                string url = "https://www.baidu.com/search?q=";
+                var request = (HttpWebRequest)WebRequest.Create(url + searchStr);
+
+                try
+                {
+                    var response = (HttpWebResponse)request.GetResponse();
+
+                    if (response != null)
+                    {
+                        Console.WriteLine(string.Format("搜索【{0}】并成功返回！", searchStr));
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+}
---
+++ ProxyPattern/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProxyPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("直接访问Google：");
+            Google google = new Google();
+            try
+            {
+                google.Search("特朗普");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.WriteLine("----------------------------");
+            Console.WriteLine("使用代理访问Google：");
+            ISearchEngine searchEngine = new GoogleProxy();
+            searchEngine.Search("特朗普");
+
+            Console.ReadLine();
+        }
+    }
+}
