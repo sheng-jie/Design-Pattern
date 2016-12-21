6f523b14e9d7afb1fcbb582545eab11239e9d21d
jeffrey <ysjshengjie@live.cn>
2016-12-21 19:39:55 +0800

    适配器模式

---
+++ AdapterPattern/IChargingLine.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdapterPattern
+{
+    /// <summary>
+    /// 充电线
+    /// 最终要适配成的目标角色
+    /// </summary>
+    public interface IChargingLine
+    {
+        /// <summary>
+        /// 充电方法
+        /// </summary>
+        void Charging();
+    }
+
+    /// <summary>
+    /// USB数据线（支持USB-Micro端口的设备）
+    /// </summary>
+    public class USBMicroLine : IChargingLine
+    {
+        public void Charging()
+        {
+            Console.WriteLine("为支持USB-Micro端口的设备充电！");
+        }
+    }
+
+    /// <summary>
+    /// 原装数据线
+    /// 未定义充电标准的充电方法
+    /// </summary>
+    public class USBLine
+    {
+        public void Charge()
+        {
+            Console.WriteLine("为设备充电！");
+        }
+    }
+
+    /// <summary>
+    /// 苹果充电线适配器
+    /// </summary>
+    public class USBlightingLineAdapter : USBLine, IChargingLine
+    {
+        public void Charging()
+        {
+            Console.WriteLine("对USB-Lighting端口的数据线进行适配！");
+            base.Charge();
+        }
+    }
+
+    /// <summary>
+    /// 小米5充电线适配器
+    /// </summary>
+    public class USBTypecLineAdapter: USBLine,IChargingLine
+    {
+        public void Charging()
+        {
+            Console.WriteLine("对USB-TypeC端口的数据线进行适配！");
+            base.Charge();
+        }
+    }
+}
---
+++ AdapterPattern/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdapterPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //在未定义充电标准之前，各个厂家充电线的实现各不相同，但都可以为自家品牌设备充电
+            USBLine usbLine = new USBLine();
+            usbLine.Charge();
+
+            Console.WriteLine("-------------------");
+            //随着电器设备越来越多，各家的充电设备不能通用，造成很多不便，为了共用，厂家联合推出标准充电接口。
+
+            //USB-Micro实现标准接口。
+            IChargingLine microLine = new USBMicroLine();
+            microLine.Charging();
+
+            Console.WriteLine("-------------------");
+
+            //现在手里有一个未实现充电标准的充电线，通过适配器，为小米5设备充电
+            IChargingLine typeCLineAdapter = new USBTypecLineAdapter();
+            typeCLineAdapter.Charging();
+
+            Console.WriteLine("-------------------");
+
+            //现在手里有一个未实现充电标准的充电线，通过适配器，为苹果设备充电
+            IChargingLine lightingLineAdapter = new USBlightingLineAdapter();
+            lightingLineAdapter.Charging();
+        }
+    }
+}
