58f931a7f70f5583d7373f58ebe6c394a1bea27f
jeffrey <ysjshengjie@live.cn>
2017-01-25 17:21:11 +0800

    观察者模式

--- ObserverPattern/SimpleImplement/FishingTool.cs
+++ ObserverPattern/SimpleImplement/FishingTool.cs
@@ -44,10 +44,13 @@ namespace ObserverPattern.SimpleImplement
         {
             Console.WriteLine("开始下钩！");
 
-            //用随机数模拟鱼咬钩，若随机数大于5，通知订阅者
-            var type = (FishType) new Random().Next(0, 5);
-            Console.WriteLine("铃铛：叮叮叮，鱼儿咬钩了");
-            Notify(type);
+            //用随机数模拟鱼咬钩，若随机数为偶数，则为鱼咬钩
+            if (new Random().Next() % 2 == 0)
+            {
+                var type = (FishType) new Random().Next(0, 5);
+                Console.WriteLine("铃铛：叮叮叮，鱼儿咬钩了");
+                Notify(type);
+            }
         }
     }
 
