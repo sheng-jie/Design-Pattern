8ce049a0473205fad1f9946597a192a0bb5e154a
shengjie_yan <ysjshengjie@live.cn>
2017-02-13 22:14:31 +0800

    迭代器模式

---
+++ IteratorPattern/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IteratorPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("迭代器模式：");
+            IListCollection list = new ConcreteList();
+            var iterator = list.GetIterator();
+
+            while (iterator.MoveNext())
+            {
+                int i = (int)iterator.GetCurrent();
+                Console.WriteLine(i.ToString());
+                iterator.Next();
+            }
+
+            Console.Read();
+        }
+    }
+}
